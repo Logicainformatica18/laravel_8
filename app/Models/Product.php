8908ec616f219195251be7c6daa857ac29be3d2c
Anthony Cardenas <logicainformatica18@gmail.com>
2022-04-05 14:34:32 -0500

    corrigiendo modelos en product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -11,21 +11,21 @@ class Product extends Model
     protected $fillable = ['id', 'description','cost','code_box','detail','providers_id','colors_id','types_id','categories_id','price1','price2','price3'];
 
     public function provider(){
-        return $this->belongsTo('App\Provider', 'providers_id');
+        return $this->belongsTo('App\Models\Provider', 'providers_id');
     }
 
     public function color()
     {
         //pertenece a muchas - agregamos el id de la tabla asociativa - pivot
-        return $this->belongsTo('App\Color','colors_id');
+        return $this->belongsTo('App\Models\Color','colors_id');
     }
     public function size(){
-        return $this->belongsTo('App\Size', 'sizes_id');
+        return $this->belongsTo('App\Models\Size', 'sizes_id');
     }
     public function type(){
-        return $this->belongsTo('App\Type', 'types_id');
+        return $this->belongsTo('App\Models\Type', 'types_id');
     }
     public function category(){
-        return $this->belongsTo('App\Category', 'categories_id');
+        return $this->belongsTo('App\Models\Category', 'categories_id');
     }
 }
